bb5a510a5c372b7a99588f029ea710b2735ae481
walerical <aldas.eric@gmail.com>
2021-05-03 23:08:01 +0200

    Dockerfile added, runtime and app port

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from src.app import app, load_database
 if __name__ == '__main__':
     load_database()
     app.debug = True
-    app.run(debug=True)
+    app.run(port=5001, debug=True, host='0.0.0.0')
