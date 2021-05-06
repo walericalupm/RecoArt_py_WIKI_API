6a65474fb3f58ec487a27e3a4002b5b5d559e793
walerical <aldas.eric@gmail.com>
2021-05-06 22:04:00 +0200

    main modified

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from src.app import app, load_database
 if __name__ == '__main__':
     load_database()
     app.debug = True
-    app.run(port=8080, debug=True, host='0.0.0.0')
+    app.run(port=80, debug=True, host='0.0.0.0')
