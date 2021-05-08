0b500420fcf4cc5d35b596033e8cbccf64505d40
walerical <aldas.eric@gmail.com>
2021-05-08 22:13:59 +0200

    flask port from Env docker var bug fixed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.app import app, load_database
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 80))
     load_database()
     app.debug = True
-    app.run(port=80, debug=True, host='0.0.0.0')
+    app.run(port=port, debug=True, host='0.0.0.0')
